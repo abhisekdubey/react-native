2493d4ec3430aa92d65777c42beb02be81a4e5ac
Abhisek Dubey <abhidb95@gmail.com>
2024-11-29 00:18:23 +0530

    Ref : color config is done also bottom tab bar navigator using rxpo router

--- first-react-native-poc/app/(tabs)/_layout.tsx
+++ first-react-native-poc/app/(tabs)/_layout.tsx
@@ -2,10 +2,22 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { FontAwesome, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 export default function _layout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: Colors.bgColor,
+          borderTopWidth: 0,
+          padding: 0,
+        },
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: Colors.black,
+        tabBarInactiveTintColor: "#999",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
@@ -26,7 +38,17 @@ export default function _layout() {
         name="search"
         options={{
           tabBarIcon: ({ color }) => (
-            <Ionicons name="search" size={28} color={color} />
+            <View
+              style={{
+                backgroundColor: Colors.primaryColor,
+                paddingHorizontal: 16,
+                paddingVertical: 12,
+                borderRadius: 10,
+                height: 50,
+              }}
+            >
+              <Ionicons name="search-outline" size={24} color={Colors.white} />
+            </View>
           ),
         }}
       />
